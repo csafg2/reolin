a2630f49952ed3349b018f52c9f60cd1ea43a91a
ads3@afgdeveloper.com <ads3@afgdeveloper.com>
2016-11-15 00:04:36 -0800

    second test

--- Reolin.DataAccess.Domain/Skill.cs
+++ Reolin.DataAccess.Domain/Skill.cs
@@ -6,7 +6,6 @@ namespace Reolin.DataAccess.Domain
 
     public class Skill
     {
-        public int SecondId { get; set; }
         public int Id { get; set; }
         public string Name { get; set; }
         public List<User> Users { get; set; }
